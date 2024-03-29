87b77146ac6ea605d0155f17271f9d124d35bcf7
Chetan Rautela <chetanrautela20@gmail.com>
2024-03-29 13:01:31 +0530

    finished level 1

--- Assets/Scripts/UI/RestartOnFall.cs
+++ Assets/Scripts/UI/RestartOnFall.cs
@@ -7,6 +7,9 @@ public class RestartOnFall : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        if (collision.gameObject.GetComponent<PlayerControler>() != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
