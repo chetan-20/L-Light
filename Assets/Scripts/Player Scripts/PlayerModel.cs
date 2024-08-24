e45b860bb571ba7152687186358698d3f71f6fa2
Chetan Rautela <chetanrautela20@gmail.com>
2024-08-24 18:42:18 +0530

    mvc works

--- Assets/Scripts/Player Scripts/PlayerModel.cs
+++ Assets/Scripts/Player Scripts/PlayerModel.cs
@@ -5,6 +5,6 @@ public class PlayerModel
     public bool isjumping = false;
     public bool issliding = false;
     public float playerhealth = 100f;
-    public float movingspeed = 3;
-    public float jumpspeed = 2;
+    public float movingspeed = 5;
+    public float jumpspeed = 3;
 }
